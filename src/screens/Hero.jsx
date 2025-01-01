d80b15741c806ff5bc01b6e9186db61860450324
Yash Dagar <59769104+yashdagar@users.noreply.github.com>
2025-01-02 01:34:59 +0530

    code of conduct

--- src/screens/Hero.jsx
+++ src/screens/Hero.jsx
@@ -4,9 +4,9 @@ const Hero = () => {
   return (
     <section
       id="hero"
-      className="hero h-svh w-full flex flex-col items-center justify-center"
+      className="hero user-drag-none h-svh w-full flex flex-col items-center justify-center"
     >
-      <img src="logos/aws.png" width="600px" loading="lazy" />
+      <img src="logos/aws.png" width="500px" loading="lazy" />
     </section>
   );
 };

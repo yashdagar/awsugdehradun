f39962f05dd2ffdd29eb36ab3864f83aa4f8a8ec
Yashpreet Singh <yashpreet1686@gmail.com>
2025-01-02 00:07:58 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-<<<<<<< HEAD
-import './App.css'
-import Body from './components/Body'
-import Header from './components/Header'
-=======
 import "./App.css";
 import Header from "./components/header/Header";
 import About from "./screens/About";
 import Hero from "./screens/Hero";
 import Speakers from "./screens/Speakers";
->>>>>>> 7e2d22c (about section implemented)
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div>
-    <Header />
-    <Body/>
-    </div>
-  )
-=======
     <>
       <Header />
       <Hero />
@@ -26,7 +13,6 @@ function App() {
       <Speakers />
     </>
   );
->>>>>>> 7e2d22c (about section implemented)
 }
 
 export default App;

981d3399e898ede7c52063b52d44fc69a014f298
yashdagar <yashdagar4557@gmail.com>
2025-04-29 09:35:25 +0530

    update about #6

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -50,9 +50,9 @@ const About = () => {
         <h4 className="mr-2">🎁</h4> Participate in fun activities and stand a chance to win exclusive AWS goodies.
         </p><br />
         <h3 className="font-semibold">EVENT DETAILS</h3>
-        <p><h4 className="mr-2">📍</h4> <b>Venue:</b> Dehradun</p><br />
-        <p><h4 className="mr-2">🗓️</h4> <b>Date:</b> Will be declared soon</p><br /> {/* TODO: EDIT AFTER REVEAL */}
-        <p><h4 className="mr-2">🕘</h4> <b>Time:</b> 9:00 AM – 6:00 PM IST</p><br /><br />
+        <p><h4 className="mr-2">📍</h4> <b>Venue:</b> Dehradun</p>
+        <p><h4 className="mr-2">🗓️</h4> <b>Date:</b> Will be declared soon</p> {/* TODO: EDIT AFTER REVEAL */}
+        <p><h4 className="mr-2">🕘</h4> <b>Time:</b> 9:00 AM – 6:00 PM IST</p>
         <a href="/code-of-conduct" className="link">
           Code Of Conduct
           <LinkIcon />
